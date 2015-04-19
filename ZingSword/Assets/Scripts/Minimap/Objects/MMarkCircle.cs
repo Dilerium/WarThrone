4a2fd6f3be6c7f61acbb19d3a0a94801932f17cc
vinilka8 <vinilka8@yahoo.com>
2015-04-19 19:59:35 +0100

    Rendered MiniMap
    Rendered minimap is ready, max and min will be added later. all mobs are
    visible in the map

---
+++ ZingSword/Assets/Scripts/Minimap/Objects/MMarkCircle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The circlur mark object in the minimap 
+/// </summary>
+public class MMarkCircle : MonoBehaviour 
+{
+    /// <summary>
+    /// Reference to the MObject associated with this class
+    /// </summary>
+    private MObject _mObj;
+
+    public MObject MObj { get { return _mObj; } set { _mObj = value; } }
+
+    void Awake()
+    {
+        if (_mObj == null) _mObj = transform.GetComponentInParentRecursively<MObject>();
+    }
+
+    void Start () 
+    {
+        animation.PlayQueued(animation.clip.name);
+    }
+    
+    void Update () 
+    {
+        //TODO animation event instead
+        if (!animation.isPlaying)
+            _mObj.SelfDestruct();
+    }
+}
