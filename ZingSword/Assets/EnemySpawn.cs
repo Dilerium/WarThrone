ec03ac7ca2f133211c9803e8b76173c69d7cfd2c
Dilerium <david.christopher.hicks@gmail.com>
2015-04-23 20:47:53 +0100

    Enemies!
    Enemies now spawn, co-ordinates addable through terrain's enemySpawner
    script.  Enemy's also approach, attack, and return to their locations.
    Damage not done!

---
+++ ZingSword/Assets/EnemySpawn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemySpawn : MonoBehaviour 
+{
+	public GameObject enemy;
+	public Vector3[] spawnPoints;
+	public bool[] spawned;
+	private List<Mob> newEnemies = new List<Mob>();
+	private static Vector3[] spawnPoints2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		for (int i=0; i < spawnPoints.Length; i++)
+		{
+			if(!spawned[i])
+			{
+				newEnemies.Add((Mob) GameObject.Instantiate (enemy.GetComponent ("Mob"), spawnPoints[i], transform.rotation));
+				spawned[i] = true;
+				Debug.Log (newEnemies[i].transform.position.ToString () + " " + enemy.ToString ());
+				newEnemies[i].setId(i);
+			}
+			/*if(newEnemies[i] == null)
+			{
+				spawned[i] = false;
+			}*/
+		}
+		spawnPoints2 = spawnPoints;
+	}
+
+	public static Vector3 getSpawn(int index)
+	{
+		return spawnPoints2[index];
+	}
+}
--- ZingSword/Assets/ZingSword/Enemies/Mobs/MobScripts/Mob.cs
+++ ZingSword/Assets/ZingSword/Enemies/Mobs/MobScripts/Mob.cs
@@ -1,51 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mob : MonoBehaviour {
-	public float speed;
-	public float range;
+public class Mob : MonoBehaviour 
+{
+	public float speed = 2;
 	public CharacterController controller;
-	public Transform player;
+	public Player player;
+	public Terrain terrain;
 
 	public AnimationClip run;
 	public AnimationClip idle;
 	public AnimationClip attack;
 	public AnimationClip die;
 
+	private int id;
 	private int health;
+	private int strength;
+	private bool hasMoved = false;
 
+	public Mob()
+	{
+
+	}
+
+	public Mob(Vector3 startPosition)
+	{
+
+	}
 
 	// Use this for initialization
+	void awake()
+	{
+		this.health = 100;
+		this.strength = 5;
+	}
+
 	void Start () 
 	{
-		health = 100;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (!inRange ()) 
+		if (inSight ())
 		{
-			chase ();		
-		} 
-		else 
+			Debug.Log ("You are being chased, zomg RUNAWAY!");
+			// If enemy has player in sight it should approach the player.
+			chase ();
+		}
+		else if(inRange ())
 		{
+			// if enemy has player in range it should attack the player.
+			doAttack ();
+		}
+		else if(Vector3.Distance (this.transform.position, EnemySpawn.getSpawn(this.id)) > 1)
+		{
+				Debug.Log ("An enemy is returning to there post!");
+				returnToPost();
+		}
+		else
+		{
+			Debug.Log ("Enemy is idle... STAB HIM!");
 			animation.CrossFade(idle.name);
 		}
-		Debug.Log (health);
 	}
 
-
-
-	bool inRange()
+	bool inSight()
 	{
 		//if distance between the position of our enemy and position of our player
-		//is less than in range of the map return true and attack
-		if (Vector3.Distance(transform.position, player.position) < range) 
+		//is less than given value return true
+		if (Vector3.Distance(this.transform.position, player.transform.position) < 15 && Vector3.Distance(this.transform.position, player.transform.position) > 2) 
 		{
 			return true;
 		} 
-		else 
+		else
+		{
+			return false;
+		}
+	}
+
+	bool inRange()
+	{
+		if (Vector3.Distance (this.transform.position, player.transform.position) < 3)
+		{
+			return true;
+		}
+		else
 		{
 			return false;
 		}
@@ -62,18 +103,51 @@ public class Mob : MonoBehaviour {
 		}
 	}
 
+	void returnToPost()
+	{
+		this.transform.LookAt (EnemySpawn.getSpawn(this.id));
+		controller.SimpleMove (this.transform.forward * speed);
+		animation.CrossFade (run.name);
+	}
+
 	//method for controlling transforms for emeny to look at player
 	void chase()
 	{
 		//look at Player
-		transform.LookAt (player.position);
+		this.transform.LookAt (player.transform.position);
 		//transform upon to player
-		controller.SimpleMove (transform.forward * speed);
+		controller.SimpleMove (this.transform.forward * speed);
 		animation.CrossFade (run.name);
 	}
 
 	void OnMouseOver()
 	{
 		player.GetComponent<Combat> ().opponent = gameObject;
+	}
+
+	void doAttack()
+	{
+		animation.CrossFade (attack.name);
+		player.takeDamage (20);
+	}
+
+	public int getId()
+	{
+		return this.id;
+	}
+
+	public void setId(int newId)
+	{
+		this.id = newId;
+	}
+
+	public string getName()
+	{
+		return name;
+	}
+
+	public void setName(string newName)
+	{
+		name = newName;
 	}
 }
